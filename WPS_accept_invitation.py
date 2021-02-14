ceedad01fba176d7dde38a2b0b375f09d565135c
igread <26396078+igread@users.noreply.github.com>
2021-02-14 14:07:55 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -8,6 +8,4 @@ sids = os.environ["SID"]
 invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
 
 for i in sids:
-    # requests.post(invite_url, headers={'sid': i}, data={'invite_userid': invite_userid})
-    print(i)
-
+    requests.post(invite_url, headers={'sid': i}, data={'invite_userid': invite_userid})
