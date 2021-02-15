a1c92b6addca6a7ecf4fa3690abcc6b063952824
igread <26396078+igread@users.noreply.github.com>
2021-02-15 20:52:37 +0800

    ~

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -1,25 +1,30 @@
-import requests
-import os
+import requests, random, os
+from time import sleep
 
 invite_userid = os.environ["USERID"]
+sids = os.environ["SIDS"]
 
-sids = [
-    "V02StVuaNcoKrZ3BuvJQ1FcFS_xnG2k00af250d4002664c02f",
-    "V02SWIvKWYijG6Rggo4m0xvDKj1m7ew00a8e26d3002508b828",
-    "V02Sr3nJ9IicoHWfeyQLiXgvrRpje6E00a240b890023270f97",
-    "V02SBsNOf4sJZNFo4jOHdgHg7-2Tn1s00a338776000b669579",
-    "V02ScVbtm2pQD49ArcgGLv360iqQFLs014c8062e000b6c37b6",
-    "V02S2oI49T-Jp0_zJKZ5U38dIUSIl8Q00aa679530026780e96",
-    "V02ShotJqqiWyubCX0VWTlcbgcHqtSQ00a45564e002678124c",
-    "V02SFiqdXRGnH5oAV2FmDDulZyGDL3M00a61660c0026781be1",
-    "V02S7tldy5ltYcikCzJ8PJQDSy_ElEs00a327c3c0026782526",
-    "V02SPoOluAnWda0dTBYTXpdetS97tyI00a16135e002684bb5c",
-    "V02Sb8gxW2inr6IDYrdHK_ywJnayd6s00ab7472b0026849b17",
-    "V02SwV15KQ_8n6brU98_2kLnnFUDUOw00adf3fda0026934a7f",
-    "V02SC1mOHS0RiUBxeoA8NTliH2h2NGc00a803c35002693584d"
-]
+invite_userid = invite_userid.split(',')
+sids = sids.split(',')
 
 invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
 
-for i in sids:
-    requests.post(invite_url, headers={'sid': i}, data={'invite_userid': invite_userid})
+n1 = 0
+n2 = 0
+for x in invite_userid:
+    for i in sids:
+        rep = requests.post(invite_url, headers={"sid": i}, data={"invite_userid": x}, timeout=10)
+        sleep(random.uniform(0.3, 1))
+        try:
+            return_result = (rep.json().get("result"))
+            if return_result == "ok":
+
+                n1 = n1 + 1
+                print("成功！ " + str(n1))
+            else:
+                n2 = n2 + 1
+                print("失败！ " + str(n2))
+        except:
+            pass
+
+print("----------\n总计:\n成功" + str(n1) + "次\n失败" + str(n2) + "次\n----------")
