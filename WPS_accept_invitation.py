7f7f03d54886c8206b0a686cbe73b0e3209701f1
igread <26396078+igread@users.noreply.github.com>
2021-02-16 20:44:54 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -16,6 +16,7 @@ for x in invite_userid:
     n1 = 0
     n2 = 0
     num1 = num1 + 1
+    mail_body = ""
     print("-----第" + str(num1) + "个ID-----")
     for i in sids:
         rep = requests.post(invite_url, headers={"sid": i}, data={"invite_userid": x}, timeout=10)
@@ -24,15 +25,23 @@ for x in invite_userid:
             return_result = (rep.json().get("result"))
             if return_result == "ok":
                 n1 = n1 + 1
+                mail_body = mail_body + "<P><STRONG>" + str(x) + "," + str(
+                    i) + ",<FONT color=#008000>成功!</FONT></STRONG></P>"
+
                 print("成功！ " + str(n1))
             else:
                 n2 = n2 + 1
+
+                mail_body = mail_body + "<P><STRONG>" + str(x) + "," + str(
+                    i) + ",<FONT color=#ff0000>失败!</FONT></STRONG></P>"
                 print("失败！ " + str(n2))
         except:
             pass
 
-mail_body = "----------\n总计:\n成功" + str(n1) + "次\n失败" + str(n2) + "次\n----------"
-print(mail_body)
+mail_body = "<P><STRONG>-------------</STRONG></P><P><STRONG>成功:<FONT color=#008000>" + str(
+    n1) + "</FONT></STRONG></P><P><STRONG>失败:<FONT color=#ff0000>" + str(
+    n2) + "</FONT></STRONG></P><P><STRONG>-------------</STRONG></P></BODY></HTML>" + mail_body
+print("----------\n总计:\n成功" + str(n1) + "次\n失败" + str(n2) + "次\n----------")
 # 发送邮件通知
 import smail
 
