098af0adb4d5f7e4c1a61109b390a89e4f40170a
igread <26396078+igread@users.noreply.github.com>
2021-02-16 08:34:42 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -1,5 +1,8 @@
-import requests, random, os
 from time import sleep
+
+import os
+import random
+import requests
 
 invite_userid = os.environ["USERID"]
 sids = os.environ["SIDS"]
@@ -9,9 +12,10 @@ sids = sids.split(',')
 
 invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
 
-n1 = 0
-n2 = 0
 for x in invite_userid:
+    n1 = 0
+    n2 = 0
+    print("-----第" + x + "个ID-----")
     for i in sids:
         rep = requests.post(invite_url, headers={"sid": i}, data={"invite_userid": x}, timeout=10)
         sleep(random.uniform(0.3, 1))
@@ -27,4 +31,4 @@ for x in invite_userid:
         except:
             pass
 
-print("----------\n总计:\n成功" + str(n1) + "次\n失败" + str(n2) + "次\n----------")
+        print("----------\n总计:\n成功" + str(n1) + "次\n失败" + str(n2) + "次\n----------")
