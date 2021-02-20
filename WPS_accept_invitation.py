c5c90b9546e9cde41ffb615e0d21484c3cf4c7e4
igread <26396078+igread@users.noreply.github.com>
2021-02-20 20:30:17 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -9,7 +9,8 @@ sids = os.environ["SIDS"]
 invite_userid = invite_userid.split(',')
 sids = sids.split(',')
 
-invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
+url = "https://zt.wps.cn/2018/clock_in/api/invite"
+payload = {'invite_userid': '1136976999'}
 
 num1 = 0
 for x in invite_userid:
@@ -19,21 +20,27 @@ for x in invite_userid:
     mail_body = ""
     print("-----第" + str(num1) + "个ID-----")
     for i in sids:
-        rep = requests.post(invite_url, headers={"sid": i}, data={"invite_userid": x}, timeout=10)
+        headers = {
+            'Accept-Language': 'zh-cn',
+            'sid': x,
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; Mi-4c Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043632 Safari/537.36 MicroMessenger/6.6.1.1220(0x26060135) NetType/WIFI Language/zh_CN MicroMessenger/6.6.1.1220(0x26060135) NetType/WIFI Language/zh_CN miniProgram'
+        }
 
+        response = requests.request("POST", url, headers=headers, data=payload, timeout=2000)
         try:
-            return_result = (rep.json().get("result"))
+            return_result = (response.json().get("result"))
+
             if return_result == "ok":
                 n1 = n1 + 1
                 mail_body = mail_body + "<P><STRONG>" + str(x) + "," + str(
                     i) + ",<FONT color=#008000>成功!</FONT></STRONG></P>"
-
                 print("成功！ " + str(n1))
             else:
+                msg = (response.json().get("msg"))
                 n2 = n2 + 1
 
                 mail_body = mail_body + "<P><STRONG>" + str(x) + "," + str(
-                    i) + ",<FONT color=#ff0000>失败!</FONT></STRONG></P>"
+                    i) + ",<FONT color=#ff0000>失败!</FONT>" + msg + "</STRONG></P>"
                 print("失败！ " + str(n2))
         except:
             pass  # 占位符
